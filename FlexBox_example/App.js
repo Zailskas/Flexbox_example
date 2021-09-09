cd82b15f0e4847b0ae528adcf131b06c59b32e63
Justinas Zailskas <j.zailskas@eif.viko.lt>
2021-09-09 10:51:32 +0300

    FlexBox system simple example

--- FlexBox_example/App.js
+++ FlexBox_example/App.js
@@ -6,107 +6,57 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Example = (props) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.example}>
+      <Text>{props.reiksme}</Text>
     </View>
-  );
-};
+  )
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={[styles.sectionContainer, { backgroundColor: '#d3d3d3' }]}>
+          <View style={styles.exampleContainer}></View>
+          <View style={styles.exampleContainer}></View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        <View style={[styles.sectionContainer, { backgroundColor: '#b0c4de'}]}>
+          <View style={[styles.exampleContainer, {alignSelf: 'flex-start'}]}></View>
+          <View style={styles.exampleContainer}></View>
+          <View style={[styles.exampleContainer, {alignSelf: 'center'}]}></View>
+        </View>
+      </SafeAreaView>        
+    )
+  }
+}
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+    flexDirection: 'column' // collumn ir row 
+  },
+  exampleContainer: {
+    backgroundColor: "#e0ffff",
+    width: 100,
+    height: 100,
+    margin: 5,
+  }, 
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    flex: 1,
+    justifyContent: "space-around",    //flex-start, flex-end, center, space-between, space-around ir space-evenly
+    alignItems: "flex-end",   //flex-start, flex-end, center, stretch ir baseline
+  }
+
 });
 
 export default App;
