6660ee01c72e0b4967888c7445e6af4dd80f27d0
Justinas Zailskas <j.zailskas@eif.viko.lt>
2021-09-30 12:25:31 +0300

    AsyncStorage example

--- Navigation_between_screens/src/firstTabPage.js
+++ Navigation_between_screens/src/firstTabPage.js
@@ -13,15 +13,17 @@ import {
     StyleSheet,
     Text,
     View,
-    TextInput, TouchableOpacity
+    TextInput, TouchableOpacity, Alert
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 class firstTabPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
+            name: '',
+            teamname: '',
         }
     }
 
@@ -31,16 +33,73 @@ class firstTabPage extends Component {
             name: value
         })
     }
+    inputChangeTeamName(value) {
+        this.setState({
+            teamname: value
+        })
+    }
+    saveData = async () => {
+        if (this.state.name === 0) {
+            Alert.alert('Insert name!!!')
+        } else {
+            try {
+                await AsyncStorage.setItem('Username', this.state.name);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+    saveObject = async () => {
+        const object = {name: this.state.name, teamname: this.state.teamname}
+        if (object === null) {
+            Alert.alert('Fill inputs')
+        } else {
+            try {
+                const jsonValue = JSON.stringify(object);
+                await AsyncStorage.setItem('Object', jsonValue);
+                console.log(jsonValue);
+            } catch (error) {
+                console.log('Neveikia');
+            }
+        }
+    }
+    getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('Username')
+            if (value !== null) {
+                console.log('Paspaudziu',value)
+            }
+        } catch (e) {
+            // error reading value
+        }
+    }
+    getObject = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('Object')
+            const value = JSON.parse(jsonValue);
+            console.log(value.name);
+            return value
+        } catch (error) {
+            //error
+        }
+    }
+
     render() {
         return (
             <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <View>
                     <Text>It is first Tab page</Text>
-                    <TextInput placeholder="Insert name" style={styles.textIinput} 
+                    <TextInput placeholder="Insert name" style={styles.textIinput}
                         onChangeText={(value) => this.inputChangeName(value)}
                     />
-                    <TouchableOpacity style={styles.buttonStyle}>
+                    <TextInput placeholder="Insert favorite team" style={styles.textIinput}
+                        onChangeText={(value) => this.inputChangeTeamName(value)}
+                    />
+                    <TouchableOpacity style={styles.buttonStyle} onPress={this.saveObject}>
                         <Text>INSERT</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonStyle} onPress={this.getObject}>
+                        <Text>SHOW</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
@@ -53,12 +112,12 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         height: 50,
         marginTop: 10
-    }, 
+    },
     buttonStyle: {
-        borderWidth: 1, 
+        borderWidth: 1,
         alignItems: 'center',
         padding: 15,
-        marginTop: 15, 
+        marginTop: 15,
         borderColor: 'green'
     }
 })
--- Navigation_between_screens/src/secondTabPage.js
+++ Navigation_between_screens/src/secondTabPage.js
@@ -6,28 +6,82 @@
  * @flow strict-local
  */
 
- import React, { Component } from 'react';
- import {
-   Button,
-   SafeAreaView,
-   StyleSheet,
-   Text,
-   View,
- } from 'react-native';
- 
+import React, { Component } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
- class secondTabPage extends Component {
-   render() {
-     return (
-       <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-         <View>
-           <Text>It is second Tab page</Text>
-         </View>
-       </SafeAreaView>
-     )
-   }
- }
- 
- 
- export default secondTabPage;
- 
+
+class secondTabPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      teamname: null
+    }
+  }
+
+  getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('Username')
+      if (value !== null) {
+        console.log('Paspaudziu', value)
+        this.setState({
+          name: value
+        })
+        console.log(this.state.name)
+      }
+    } catch (e) {
+      console.log('neveikia')
+    }
+  }
+  getObject = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('Object')
+        const value = JSON.parse(jsonValue);
+        console.log(value.name);
+        this.state({
+          name: value.name,
+          teamname: value.teamname
+        })
+        return value
+    } catch (error) {
+        //error
+    }
+}
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View>
+          <Text>It is second Tab page</Text>
+          <Text>{this.state.name} palaiko {this.state.teamname}</Text>
+          <TouchableOpacity style={styles.buttonStyle} onPress={this.getObject}>
+            <Text>SHOW</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    borderWidth: 1,
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 15,
+    borderColor: 'green'
+  }
+})
+
+export default secondTabPage;
